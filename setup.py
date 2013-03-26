991e0f6692a5a07eda8bd1901af6110b5a7dee0b
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-26 16:48:52 -0400

    Add more specific Python version classifiers

--- setup.py
+++ setup.py
@@ -28,6 +28,10 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
     ],
 )
