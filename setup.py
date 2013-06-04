dc92f9543f1389ab33d0b7b4602b818a3a634613
Kenn Knowles <kenn.knowles@gmail.com>
2013-06-04 14:44:58 -0400

    Do not use smartquotes, since unicode breaks Windows

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess
 
 # Build README.txt from README.md if not present, and if we are actually building for distribution to pypi
 if not os.path.exists('README.txt') and 'sdist' in sys.argv:
-    subprocess.call(['pandoc', '--to=rst', '--smart', '--output=README.txt', 'README.md'])
+    subprocess.call(['pandoc', '--to=rst', '--output=README.txt', 'README.md'])
 
 # But use the best README around; never fail - there are some Windows locales that seem to die on smartquotes,
 # even with the explicit encoding
